9e77acdfb9cd5227144188b8414713d34edba06e
DavidTorres-sys <david.torresg@udea.edu.co>
2024-05-22 15:12:57 -0500

    :alien: Db script
    }

--- src/main/java/com/sitas/checkin/services/email/service/EmailServiceImpl.java
+++ src/main/java/com/sitas/checkin/services/email/service/EmailServiceImpl.java
@@ -4,4 +4,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class EmailServiceImpl implements IEmailService {
+    @Override
+    public void sendEmail(String userEmail) {
+    }
 }
--- src/main/java/com/sitas/checkin/services/email/service/IEmailService.java
+++ src/main/java/com/sitas/checkin/services/email/service/IEmailService.java
@@ -1,4 +1,13 @@
 package com.sitas.checkin.services.email.service;
 
+/**
+ * This interface defines methods for sending emails.
+ */
 public interface IEmailService {
+    /**
+     * Sends an email to the specified user email address.
+     *
+     * @param userEmail the email address of the user to whom the email will be sent
+     */
+    public void sendEmail(String userEmail);
 }
